f935d14ac226394416d1c00f56007e94625dcb60
Ario93 <Ariodw44@gmail.com>
2024-05-14 23:20:09 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,13 +2,10 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open('v1').then((cache) => {
       return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/script.js',
-        '/manifest.json',
-        '/Alive-Black (1).png',
-        '/Alive-Black (1).png'
+        'https://alive2024.blogspot.com/',
+        'https://ario93.github.io/manifest.json',
+        'https://ario93.github.io/Alive-Black (1).png',
+        'https://ario93.github.io/Alive-Black (1).png'
       ]);
     })
   );
