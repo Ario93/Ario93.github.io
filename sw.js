2e045b02f9229d0cafbd0ab73435b6d57e61db8a
Ario93 <Ariodw44@gmail.com>
2024-05-14 22:37:30 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/script.js',
+        '/manifest.json',
+        '/icons/javascript2.png',
+        '/icons/javascript2.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
